3182a3a417f8a595a24d078da8ac60988ea727dd
tyjryhtfhmfddmf-hub <tyjryhtfhmfddmf@gmail.com>
2025-10-23 23:40:31 +0100

    Update v5.5.py

--- v5.5.py
+++ v5.5.py
@@ -17,7 +17,7 @@ from tkinter import simpledialog, messagebox
 # -----------------------------
 root = TkinterDnD.Tk()
 root.title("Music Sync App")
-root.geometry("500x600")  # Optional: Set window size
+root.geometry("500x1200")  # Optional: Set window size
 root.resizable(False, False)
 
 # ---------------------------------------
